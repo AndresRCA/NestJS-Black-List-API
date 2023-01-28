5968621d8469b60612129e36e9c53db99e1fdce6
AndresRCA <andresrca97@gmail.com>
2023-01-28 13:58:37 -0400

    remove deprecated files

--- __tests__/helper_classes/Auth.test.ts
+++
@@ -1,7 +0,0 @@
-import { Auth } from '../../src/helper_classes/Auth'
-
-describe('is_request_valid', () => {
-    it('should not take an invalid token value', () => {
-        expect(Auth.is_request_valid('invalid token')).toBe(false)
-    })
-})
