8b32cc0e32a178cfa522b519411647a9cbaf22a5
AndresRCA <andresrca97@gmail.com>
2023-01-28 13:55:15 -0400

    add security middleware (helmet + cors)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import { router as black_list_routes } from "./routes/black_list.routes";
+import { router as black_list_routes } from "./routes/black_list.routes"
 import { validate_auth_token } from './middleware/check_auth.middleware'
+import helmet from 'helmet'
+import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -9,6 +11,10 @@ app.use(bodyParser.json())
 
 // setup the middleware functions
 app.use(validate_auth_token)
+
+// setup security and access measures
+app.use(helmet())
+app.use(cors({ origin: '*' }))
 
 // setup routes that hold their respective controllers
 app.use('/black_list', black_list_routes)
